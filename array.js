f4a52115ec2dc45ff1b866c60ed591cfdd234a79
juwel164 <juwelhossain16457@gmail.com>
2026-08-24 22:59:07 +0600

    array metheod(push, pop, unshift, shift, splice, slice)

--- array.js
+++ array.js
@@ -1,3 +1,15 @@
 const names = ["Shafayet", "Rakib", "Mir", "Mezbah"];
 const numbers = [100, 500, 600, 300];
 console.log(names[0], numbers[3]);
+
+names.push("Juwel");
+names.push("Mafi");
+names.pop();
+
+names.unshift("Kajol");
+names.shift();
+
+names.splice(2, 1, "Kuddus");
+
+
+console.log(names);
