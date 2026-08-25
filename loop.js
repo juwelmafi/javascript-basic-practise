8ee1576cc32b469de4f5efefd96b870af7a3477c
juwel164 <juwelhossain16457@gmail.com>
2026-08-25 22:43:50 +0600

    loop practice

---
+++ loop.js
@@ -0,0 +1,22 @@
+for(let i = 0; i <= 5; i++){
+  console.log("Count: ", i);
+}
+
+
+
+for(let i = 5; i >= 0; i--){
+  console.log("Countdown: ", i);
+}
+
+const arr = ["Apple", "Mango", "Lichi", "Orange", "Banana"];
+
+for(let i = 0; i < arr.length; i++){
+  console.log("KHABO", arr[i]);
+}
+
+
+let i = 0;
+while(i < arr.length){
+  console.log("khabo: ", arr[i]);
+  i++;
+}
