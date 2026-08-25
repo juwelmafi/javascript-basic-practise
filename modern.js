51c149c8c24b7f064c4196db2544ec6f4e890f8b
juwel164 <juwelhossain16457@gmail.com>
2026-08-25 23:40:43 +0600

    moder js loops

---
+++ modern.js
@@ -0,0 +1,33 @@
+// const techStack = ["React", "Next.js", "MongoDB", "PostgreSQL"];
+
+// for(let tech of techStack){
+//   console.log("Learning: ", tech);
+// }
+
+
+// const obj = {
+//   name: "Ashik",
+//   age: 32,
+//   address: "Dhaka"
+// }
+
+// for(ele in obj){
+//   console.log(ele, ">>>", obj[ele]);
+// }
+
+
+// const marks = 50;
+
+// for(i = marks; i <= 100; i++){
+//   if(i > 60){
+//     break;
+//   }
+//   console.log(i);
+// }
+
+for(let i = 1; i <= 15; i++){
+  if(i % 2 != 0){
+    continue;
+  }
+  console.log(i);
+}
